56c394bcaa6dd3a6761beaa6e0ea13a6ac90eef4
Danila-hub <kontaigabox@mail.ru>
2022-04-27 21:41:55 +0300

    Added header and slider

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,14 @@
 //import * as flsFunctions from "./modules/functions.js";
 //flsFunctions.thisTest();
 
+function ibg() {
+  let ibg = document.querySelectorAll('.ibg');
+  for (var i = 0; i < ibg.length; i++) {
+    if (ibg[i].querySelector('img')) {
+      ibg[i].style.backgroundImage =
+        'url(' + ibg[i].querySelector('img').getAttribute('src') + ')';
+    }
+  }
+}
 
+ibg();
