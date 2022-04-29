c5123144ffc6d2c19e3d87461579182ba0123cb3
Danila-hub <kontaigabox@mail.ru>
2022-04-29 17:48:18 +0300

    Added first block and burger menu + header

--- src/js/app.js
+++ src/js/app.js
@@ -12,3 +12,14 @@ function ibg() {
 }
 
 ibg();
+
+//burger
+const iconMenu = document.querySelector('.menu__icon');
+if (iconMenu) {
+  const menuBody = document.querySelector('.header__menu');
+  iconMenu.addEventListener('click', function (e) {
+    document.body.classList.toggle('lock');
+    iconMenu.classList.toggle('active');
+    menuBody.classList.toggle('active');
+  });
+}
